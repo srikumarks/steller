26bbbf77b419d01edc9eb05bb56eb120df6d98a3
Srikumar K. S <srikumarks@gmail.com>
2012-10-09 05:52:27 +0530

    Added a spectrum analyzer model that wraps the RealtimeAnalyserNode. This model does periodic spectrum analysis, unlike the builtin node from which you got to request a spectrum scan.

--- models.js
+++ models.js
@@ -250,6 +250,71 @@ function (sh) {
         };
     }());
 
+    // A simple wrapper around the realtime analyzer node that fetches the
+    // magnitude spectrum (converting from decibels) periodically. The optional
+    // argument gives the number of bins desired (the fft size is double this).
+    // The bin count defaults to 1024 if unspecified.
+    //
+    // .bins is a Float32Array that will be kept updated with the power spectrum.
+    // .freqs is a constant Float32Array containing the frequency value of each bin.
+    // .time is a parameter whose value gives the time at which the spectrum was grabbed last.
+    //      You can 'watch' this parameter to be notified of each grab.
+    //
+    // You must run the .start action to start grabbing. You can stop 
+    // using the .stop action.
+    models.spectrum = function (N) {
+        N = N || 1024;
+
+        var analyser = AC.createAnalyser();
+        analyser.fftSize = N * 2;
+        analyser.frequencyBinCount = N;
+        analyser.smoothingTimeConstant = 0;
+        // Note that the analyser doesn't need to be connected to AC.destination.
+
+        var model = SoundModel({}, [analyser], []);
+        model.bins = new Float32Array(N);
+        model.freqs = new Float32Array(N);
+        model.time = Param({min: 0, max: 1e9, value: 0});
+
+        // Compute the frequencies of the bins.
+        for (var i = 0; i < N; ++i) {
+            model.freqs[i] = i * AC.sampleRate / analyser.fftSize;
+        }
+
+        var grabRequest;
+
+        function update() {
+            if (grabRequest) {
+                var ts = AC.currentTime;
+                analyser.getFloatFrequencyData(model.bins);
+
+                // Convert from decibels to power.
+                for (var i = 0, bins = model.bins, N = bins.length; i < N; ++i) {
+                    bins[i] = Math.pow(10, bins[i] / 20);
+                }
+
+                grabRequest = steller.requestAnimationFrame(update);
+
+                // Update the time stamp. If there are watchers installed
+                // on model.time, they'll now be notified of the frame grab.
+                model.time.value = ts;
+            }
+        }
+
+        model.start = sh.fire(function () {
+            if (!grabRequest) {
+                grabRequest = steller.requestAnimationFrame(update);
+            }
+        });
+
+        model.stop = sh.fire(function () {
+            grabRequest = undefined;
+        });
+
+        return model;
+    };
+
+
     // A simple "load and play sample" model that will load the given url when
     // the .load action is run, and can play the sound from start to finish.
     // Creating multiple sample models from the same URL will not incur
