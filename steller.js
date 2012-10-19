a3d3892bdad274062c25ab7214f06a4b8e4ee1bf
Srikumar K. S <srikumarks@gmail.com>
2012-10-19 07:13:02 +0530

    Updated license to LGPL v2 or above.

--- steller.js
+++ steller.js
@@ -1,8 +1,11 @@
-// Copyright (c) 2012 Arts and Creativity Lab, Singapore
-// http://anclab.org
+// Copyright (c) 2012 National University of Singapore
+// Inquiries: director@anclab.org
+// Author: Srikumar K. S. (http://github.com/srikumarks)
+//
 // #### License
+//
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
+// it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
 //
@@ -11,8 +14,8 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/lgpl.html>.
 
 // ## Introduction
 //
