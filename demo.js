0c069e042b257984e1b354534fbb8e6b533e0bac
Srikumar K. S <srikumarks@gmail.com>
2020-03-25 20:23:06 +0530

    Updated demo so it will work fine with new browser constraints.
    Browsers now have the constraint that sound will be generated by
    AudioContext only if it is created in response to a user event.
    So now all the setup is done inside a START() function call
    which is called in response to a button click event.

--- demo.js
+++ demo.js
@@ -1,159 +1,162 @@
 // Some simple examples to show usage of the Steller framework.
 // https://github.com/srikumarks/steller
 
+function START() {
 
-// Some helpers for the demos.
-var steller = org.anclab.steller;       // Alias for namespace.
+    document.getElementById('demo_section').removeAttribute('hidden');
 
-var AC = new steller.AudioContext(); // Make an audio context.
+    // Some helpers for the demos.
+    var steller = org.anclab.steller;       // Alias for namespace.
 
-var sh = new steller.Scheduler(AC);     // Create a scheduler and start it running.
-sh.running = true;
+    var AC = new steller.AudioContext(); // Make an audio context.
 
-// Shorthand for DOM element access.
-function elem(id) {
-    return document.getElementById(id);
-}
+    var sh = new steller.Scheduler(AC);     // Create a scheduler and start it running.
+    sh.running = true;
 
-// The "ting" model plays a tone with an exponential decay. This component
-// is designed so that it can play multiple "ting"s through the output of
-// the same graph node. You may want this in some circumstances, and not in
-// others and both are expressible using the Steller framework.
-//
-// @param sh is a Scheduler instance.
-function ting(sh) {
-
-    var output = AC.createGainNode();
-    var model = steller.SoundModel({}, [], [output]);
-
-    // halfLife parameter determines the amplitude decay half life (in secs) of
-    // a ting at 440Hz.
-    model.halfLife = steller.Param({min: 0.001, max: 10, value: 0.5});
-    
-    function play(pitchNumber) {
-        return sh.fire(function (clock) {
-            var f = 440 * Math.pow(2, (pitchNumber.valueOf() - 69) / 12);
-            var osc = AC.createOscillator();
-            osc.type = osc.SINE;
-            osc.frequency.value = f;
-
-            var gain = AC.createGainNode();
-            osc.connect(gain);
-            gain.connect(output);
-            gain.gain.setValueAtTime(1/8, clock.t1);
-
-            var halfLife = model.halfLife.value * 440 / f;
-            var dur = halfLife * 12;
-            gain.gain.exponentialRampToValueAtTime(1/32768, clock.t1 + dur);
-            osc.noteOn(clock.t1);
-            osc.noteOff(clock.t1 + dur);
-        });
+    // Shorthand for DOM element access.
+    function elem(id) {
+        return document.getElementById(id);
     }
 
-    // You can play multiple tings all mixed into the same output gain node.
-    // Note that there is no standard way to "play" or "stop" any sound model.
-    // This is left open since models may need different behaviours in this
-    // regard. For this model, `.play(noteNumber)` is a method that makes
-    // an action meant to be passed to the scheduler.
-    model.play = play;
+    // The "ting" model plays a tone with an exponential decay. This component
+    // is designed so that it can play multiple "ting"s through the output of
+    // the same graph node. You may want this in some circumstances, and not in
+    // others and both are expressible using the Steller framework.
+    //
+    // @param sh is a Scheduler instance.
+    function ting(sh) {
 
-    return model;
-} 
+        var output = AC.createGainNode();
+        var model = steller.SoundModel({}, [], [output]);
 
-elem('ting').onclick = function (event) {
-    var t = ting(sh);
-    t.connect(AC.destination);
+        // halfLife parameter determines the amplitude decay half life (in secs) of
+        // a ting at 440Hz.
+        model.halfLife = steller.Param({min: 0.001, max: 10, value: 0.5});
 
-    // Play an intro chime.
-    var hello = sh.track([60, 64, 67, 72].map(function (p) { 
-        return sh.track(t.play(p), sh.delay(0.25));
-    }));
+        function play(pitchNumber) {
+            return sh.fire(function (clock) {
+                var f = 440 * Math.pow(2, (pitchNumber.valueOf() - 69) / 12);
+                var osc = AC.createOscillator();
+                osc.type = osc.SINE;
+                osc.frequency.value = f;
 
-    sh.play(hello);
-};
+                var gain = AC.createGainNode();
+                osc.connect(gain);
+                gain.connect(output);
+                gain.gain.setValueAtTime(1/8, clock.t1);
 
-// The arpeggio model plays a sequence of tones in a loop.  The loop is
-// started/stopped using the "start" and "stop" buttons in the UI.
-function arpeggio(sh) { 
-    
-    var t = ting(sh);
+                var halfLife = model.halfLife.value * 440 / f;
+                var dur = halfLife * 12;
+                gain.gain.exponentialRampToValueAtTime(1/32768, clock.t1 + dur);
+                osc.noteOn(clock.t1);
+                osc.noteOff(clock.t1 + dur);
+            });
+        }
 
-    // The "tinger" is itself the output node for this model.
-    var model = steller.SoundModel({}, [], [t]);
+        // You can play multiple tings all mixed into the same output gain node.
+        // Note that there is no standard way to "play" or "stop" any sound model.
+        // This is left open since models may need different behaviours in this
+        // regard. For this model, `.play(noteNumber)` is a method that makes
+        // an action meant to be passed to the scheduler.
+        model.play = play;
 
-    // Sequenced tones.
-    var arp = sh.track( t.play(60), sh.delay(1)
-                      , t.play(64), sh.delay(1)
-                      , t.play(67), sh.delay(1)
-                      , t.play(69), sh.delay(1)
-                      , t.play(67), sh.delay(1)
-                      , t.play(64), sh.delay(1)
-                      );
+        return model;
+    } 
 
-    // The "playing" parameter indicates (and controls) whether
-    // the sequence is playing or not.
-    model.playing = steller.Param({min: 0, max: 1, value: 0});
+    elem('ting').onclick = function (event) {
+        var t = ting(sh);
+        t.connect(AC.destination);
 
-    // Also expose the "halfLife" parameter of the tones being used.
-    // Usually `exposeAs` will be used to give a different name according
-    // to the perceptual quality of the parameter in the context of this
-    // model, but in this case we just expose it as is.
-    model.halfLife = t.halfLife;
+        // Play an intro chime.
+        var hello = sh.track([60, 64, 67, 72].map(function (p) { 
+            return sh.track(t.play(p), sh.delay(0.25));
+        }));
 
-    // An action that sets playing state to "playing".
-    model.start = sh.fire(function () {
-        model.playing.value = 1;
-    });
-   
-    // An action that sets playing state to "not playing".  The stopping will
-    // always occur at the end of a complete arpeggio sequence.
-    model.stop = sh.fire(function () {
-        model.playing.value = 0;
-    });
+        sh.play(hello);
+    };
 
-    // The pattern to play.
-    model.pattern = sh.loop_while(model.playing, arp);
+    // The arpeggio model plays a sequence of tones in a loop.  The loop is
+    // started/stopped using the "start" and "stop" buttons in the UI.
+    function arpeggio(sh) { 
 
-    return model;
+        var t = ting(sh);
+
+        // The "tinger" is itself the output node for this model.
+        var model = steller.SoundModel({}, [], [t]);
+
+        // Sequenced tones.
+        var arp = sh.track( t.play(60), sh.delay(1)
+        , t.play(64), sh.delay(1)
+        , t.play(67), sh.delay(1)
+        , t.play(69), sh.delay(1)
+        , t.play(67), sh.delay(1)
+        , t.play(64), sh.delay(1)
+        );
+
+        // The "playing" parameter indicates (and controls) whether
+        // the sequence is playing or not.
+        model.playing = steller.Param({min: 0, max: 1, value: 0});
+
+        // Also expose the "halfLife" parameter of the tones being used.
+        // Usually `exposeAs` will be used to give a different name according
+        // to the perceptual quality of the parameter in the context of this
+        // model, but in this case we just expose it as is.
+        model.halfLife = t.halfLife;
+
+        // An action that sets playing state to "playing".
+        model.start = sh.fire(function () {
+            model.playing.value = 1;
+        });
+
+        // An action that sets playing state to "not playing".  The stopping will
+        // always occur at the end of a complete arpeggio sequence.
+        model.stop = sh.fire(function () {
+            model.playing.value = 0;
+        });
+
+        // The pattern to play.
+        model.pattern = sh.loop_while(model.playing, arp);
+
+        return model;
+    }
+
+    (function () {
+        var arp = arpeggio(sh);
+        arp.connect(AC.destination); // We can optionally pipe this through effects before
+        // hitting the destination node.
+
+        // Make a parameter for controlling the rate of playback.
+        // We'll set the rate "by reference" so that changes to it
+        // can take effect between individual notes of the arpeggio.
+        var p = {};
+        p.rate = steller.Param({min: 0.001, max: 1000, value: 1});
+
+        elem('arpeggio_start').onclick = function (event) {
+            // Note that you can click "start" again while a sequence is playing
+            // to start another "in parallel", though all such sequences will be
+            // controlled using the same rate and half life parameters and all
+            // of them will be stopped by clicking "stop".
+            sh.play(sh.track(sh.rate(p.rate), arp.start, arp.pattern));
+        };
+
+        elem('arpeggio_stop').onclick = function (event) {
+            sh.play(arp.stop);
+        };
+
+        elem('ting_halflife').oninput = function (event) {
+            var value = parseFloat(this.value);
+            var halfLife = Math.pow(10, value);
+            elem('ting_halflife_value').innerText = '' + (Math.round(halfLife * 1000) / 1000) + ' secs';
+            arp.halfLife.value = halfLife;
+        };
+
+        elem('arpeggio_speed').oninput = function (event) {
+            var value = parseFloat(this.value);
+            var rate = Math.pow(10, value);
+            elem('arpeggio_speed_value').innerText = '' + (Math.round(rate * 100) / 100);
+            p.rate.value = rate;
+        };
+    }());
+
+
 }
-
-(function () {
-    var arp = arpeggio(sh);
-    arp.connect(AC.destination); // We can optionally pipe this through effects before
-                                // hitting the destination node.
-
-    // Make a parameter for controlling the rate of playback.
-    // We'll set the rate "by reference" so that changes to it
-    // can take effect between individual notes of the arpeggio.
-    var p = {};
-    p.rate = steller.Param({min: 0.001, max: 1000, value: 1});
-
-    elem('arpeggio_start').onclick = function (event) {
-        // Note that you can click "start" again while a sequence is playing
-        // to start another "in parallel", though all such sequences will be
-        // controlled using the same rate and half life parameters and all
-        // of them will be stopped by clicking "stop".
-        sh.play(sh.track(sh.rate(p.rate), arp.start, arp.pattern));
-    };
-
-    elem('arpeggio_stop').onclick = function (event) {
-        sh.play(arp.stop);
-    };
-
-    elem('ting_halflife').onchange = function (event) {
-        var value = parseFloat(this.value);
-        var halfLife = Math.pow(10, value);
-        elem('ting_halflife_value').innerText = '' + (Math.round(halfLife * 1000) / 1000) + ' secs';
-        arp.halfLife.value = halfLife;
-    };
-
-    elem('arpeggio_speed').onchange = function (event) {
-        var value = parseFloat(this.value);
-        var rate = Math.pow(10, value);
-        elem('arpeggio_speed_value').innerText = '' + (Math.round(rate * 100) / 100);
-        p.rate.value = rate;
-    };
-}());
-
-
-
